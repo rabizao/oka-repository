f652cd0140c4b72485a284c10cea6cc918591923
Rafael A. Bizao <rabizao@gmail.com>
2020-08-12 15:37:12 -0300

    download route

--- backend/app/api/downloads.py
+++ backend/app/api/downloads.py
@@ -1,13 +1,15 @@
 from . import bp
 from flask.views import MethodView
 from app.schemas import DownloadQuerySchema
+from flask import send_from_directory, current_app
 
 
 @bp.route('/downloads')
 class Users(MethodView):
     # @jwt_required
     @bp.arguments(DownloadQuerySchema, location="query")
-    @bp.response(code=200)
     def get(self, args):
         """Download a zipped file containing all the requested datasets"""
         print(args['uuids'])
+        return send_from_directory(current_app.static_folder,
+                                   "iris.arff", as_attachment=True)
