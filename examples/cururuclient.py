cc6397d907793e8364606fdfd6f354dfe1e6103f
davips <dpsabc@gmail.com>
2020-09-07 14:44:34 -0300

    Shared token between tests (cururuclient and okatest)

--- examples/cururuclient.py
+++ examples/cururuclient.py
@@ -1,3 +1,5 @@
+import json
+
 from cururu.okaserver import OkaServer
 from pjdata.content.specialdata import UUIDData
 from pjdata.creation import read_arff
@@ -5,6 +7,8 @@ from util.create import user, token
 
 print("Create user and token...")
 token = token(*user("okatest", "pass123")[0:2])
+with open("token.txt", "w") as f:
+    json.dump({"token": token}, f)
 
 storage = OkaServer(post=True, token=token)
 
