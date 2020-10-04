028f5220a47dc279bff9840bcbeefa44c69f8664
Rafael Bizao <rabizao@gmail.com>
2020-10-04 17:33:59 -0300

    logout all devices

--- frontend/src/components/OkaMyAccount/index.js
+++ frontend/src/components/OkaMyAccount/index.js
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom';
 
 import Avatar from 'react-avatar';
 import { ExpandMore } from '@material-ui/icons';
+import { NotificationManager } from 'react-notifications';
 
 import { LoginContext } from '../../contexts/LoginContext';
 import { logout } from '../../services/auth';
 import PopOver from '../PopOver';
+import api from '../../services/api';
+
 
 export default function OkaMyAccount() {
     const loggedUser = useContext(LoginContext);
@@ -15,6 +18,21 @@ export default function OkaMyAccount() {
         logout();
         window.location.href = '/';
         return
+    }
+
+    async function handleLogoutAllDevices() {
+        try {
+            await api.delete('/auth/revoke-all-tokens');
+            handleLogout();
+        } catch (error) {
+            if (error.response) {
+                for (var prop in error.response.data.errors.json) {
+                    NotificationManager.error(error.response.data.errors.json[prop], `${prop}`, 4000)
+                }
+            } else {
+                NotificationManager.error("network error", "error", 4000)
+            }
+        }
     }
 
     return (
@@ -28,7 +46,8 @@ export default function OkaMyAccount() {
                     <Link className="margin-top-small" to={`/users/${loggedUser.username}/uploads`}><h1 className="ellipsis-15ch">{loggedUser.name}</h1></Link>
                     <Link className="flex-row flex-crossaxis-center margin-top-medium padding-vertical-small box background-hover width100" to={`/users/${loggedUser.username}/uploads`}>Uploads/Favorites</Link>
                     <Link className="flex-row flex-crossaxis-center padding-vertical-small box background-hover width100" to={`/client`}>Oka Client</Link>
-                    <button onClick={handleLogout} className="margin-bottom-small padding-vertical-small box background-hover width100">Logout</button>
+                    <button onClick={handleLogout} className="padding-vertical-small box background-hover width100">Logout</button>
+                    <button onClick={handleLogoutAllDevices} className="margin-bottom-small padding-vertical-small box background-hover width100">Logout From All Devices</button>
                 </div>
             }
         />
