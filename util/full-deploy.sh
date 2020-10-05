c4cb988b8f25b7f7b3f4e309e432ba9062023ffb
oka <you@example.com>
2020-10-04 22:12:45 -0300

    Add full-deploy script

---
+++ util/full-deploy.sh
@@ -0,0 +1,15 @@
+cd /home/oka/oka-repository/ && sudo -u oka git pull; cd -
+cd /home/oka/tatu/ && sudo -u oka git pull; cd -
+cd /home/oka/akangatu/ && sudo -u oka git pull; cd -
+cd /home/oka/transf/ && sudo -u oka git pull; cd -
+cd /home/oka/aiuna/ && sudo -u oka git pull; cd -
+cd /home/oka/kururu/ && sudo -u oka git pull; cd -
+cd /home/oka/cruipto/ && sudo -u oka git pull; cd -
+cd /home/oka/oka-repository/backend
+source venv/bin/activate
+flask db migrate
+flask db upgrade
+cd -
+echo "Restart services..."
+systemctl restart oka nginx celery
+
