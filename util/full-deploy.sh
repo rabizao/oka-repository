b2df20e3216f495f2d2063987768afe0fd493237
oka <you@example.com>
2020-10-09 09:00:31 -0300

    deploy file

--- util/full-deploy.sh
+++ util/full-deploy.sh
@@ -5,6 +5,7 @@ cd /home/oka/transf/ && sudo -u oka git pull; cd -
 cd /home/oka/aiuna/ && sudo -u oka git pull; cd -
 cd /home/oka/kururu/ && sudo -u oka git pull; cd -
 cd /home/oka/cruipto/ && sudo -u oka git pull; cd -
+cd /home/oka/oka-repository/frontend && sudo -u oka npm run-script build; cd -
 cd /home/oka/oka-repository/backend
 source venv/bin/activate
 flask db migrate
