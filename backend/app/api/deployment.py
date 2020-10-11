37bfed8fb34bc7037953906716c35169784f11dd
Rafael Bizao <rabizao@gmail.com>
2020-10-11 10:19:50 -0300

    deploy secret verify

--- backend/app/api/deployment.py
+++ backend/app/api/deployment.py
@@ -1,6 +1,6 @@
 from . import bp
 
-from flask import request
+from flask import request, current_app
 from flask.views import MethodView
 from flask_smorest import abort
 from werkzeug.security import check_password_hash
@@ -11,10 +11,13 @@ class Deployment(MethodView):
     @bp.response(code=200)
     def post(self):
         """
-        Deploy the application after a github webhook
+        Deploy the application using github webhook
         """
         if 'X-Hub-Signature' not in request.headers:
             abort(422, errors={
                 "headers": {"X-Hub-Signature": ["Required header signature. [" + self.__class__.__name__ + "]"]}})
-        print(check_password_hash("teste", "teste2"))
-        print("run scripts")
+        if not check_password_hash(str(request.headers['X-Hub-Signature']),
+                                   current_app.config['DEPLOYMENT_SECRET_KEY']):
+            abort(422, errors={
+                "headers": {"X-Hub-Signature": ["Invalid secret key. [" + self.__class__.__name__ + "]"]}})
+        print("run reploy scripts")
