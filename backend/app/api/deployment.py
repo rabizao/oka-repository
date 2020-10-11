82c58251bbe955fb01899d13244fa9af30b4523a
Rafael Bizao <rabizao@gmail.com>
2020-10-11 10:58:40 -0300

    github pass check test

--- backend/app/api/deployment.py
+++ backend/app/api/deployment.py
@@ -3,7 +3,8 @@ from . import bp
 from flask import request, current_app
 from flask.views import MethodView
 from flask_smorest import abort
-from werkzeug.security import check_password_hash
+import hashlib
+import hmac
 
 
 @bp.route('/deployment')
@@ -16,8 +17,10 @@ class Deployment(MethodView):
         if 'X-Hub-Signature' not in request.headers:
             abort(422, errors={
                 "headers": {"X-Hub-Signature": ["Required header signature. [" + self.__class__.__name__ + "]"]}})
-        if not check_password_hash(str(request.headers['X-Hub-Signature']),
-                                   current_app.config['DEPLOYMENT_SECRET_KEY']):
+
+        if not hmac.compare_digest(request.headers.get('X-Hub-Signature').split('sha1=')[-1].strip(),
+                                   hmac.new(key=bytes(current_app.config['DEPLOYMENT_SECRET_KEY'], 'utf-8'),
+                                            msg=request.data, digestmod=hashlib.sha1).hexdigest()):
             abort(422, errors={
                 "headers": {"X-Hub-Signature": ["Invalid secret key. [" + self.__class__.__name__ + "]"]}})
         print("run reploy scripts")
