bec4c56a403ba450ad76f23b48ecccce2df0bc90
Rafael Bizao <rabizao@gmail.com>
2020-10-11 10:08:30 -0300

    get deploy header

--- backend/app/api/deployment.py
+++ backend/app/api/deployment.py
@@ -2,6 +2,7 @@ from . import bp
 
 from flask import request
 from flask.views import MethodView
+from flask_smorest import abort
 from werkzeug.security import check_password_hash
 
 
@@ -12,6 +13,8 @@ class Deployment(MethodView):
         """
         Deploy the application after a github webhook
         """
-        print(request.headers)
+        if 'X-Hub-Signature' not in request.headers:
+            abort(422, errors={
+                "headers": {"X-Hub-Signature": ["Required header signature. [" + self.__class__.__name__ + "]"]}})
         print(check_password_hash("teste", "teste2"))
         print("run scripts")
