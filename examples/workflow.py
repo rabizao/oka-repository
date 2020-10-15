4baf4490acfb261eb31b8570afe17439da2b3989
davips <dpsabc@gmail.com>
2020-10-15 07:11:12 -0300

    Move Let to aiuna

--- examples/workflow.py
+++ examples/workflow.py
@@ -56,15 +56,15 @@ from util.create import user, token
 url = "http://localhost:5000"
 user = user("davips", "pass123", base_url=url)
 okatoken = token(**user, base_url=url)
-print("user created")
+print("user created", okatoken)
 
 # TODO: multiple caches are not working regarding whether to post
 # TIP: TsSplit should come before TrSplit to ensure the same original data is used as input for both.
-from tatu.okast import OkaSt
+# from tatu.okast import OkaSt
 
-oka = OkaSt(okatoken, alias="Iris", url=url)
+# oka = OkaSt(okatoken, alias="Iris", url=url)
 sq = SQLite()
-my = MySQL(db="oka:xxxxxx@localhost/oka")
+# my = MySQL(db="oka:kururu@localhost/oka")
 #
 wflow = (
         File("iris.arff")
@@ -72,28 +72,30 @@ wflow = (
         * Split
         * PCA(n=3)
         * Cache(PCA(n=3), storage=sq)
-        * Cache(PCA(n=3), storage=oka)
+        # * Cache(PCA(n=3), storage=oka)
         * PCA(n=3)
         * Log(">>>>>>>>>>>>>>>>> {X.shape} {inner.X.shape}")
         * Report("{id}")
-        * Cache(SVM2(C=0.25), storage=my)
-        * Metric2
-        * Report("tr {r}\t\tts {inner.r}")
+    # * Cache(SVM2(C=0.25), storage=my)
+    # * Metric2
+    # * Report("tr {r}\t\tts {inner.r}")
 )
 
 data = wflow.data
 
 print("Data ID", data.id)
-print("Shape", data.X.shape[1], len(data.Xt))
 
-# print(test.arff("nome", "desc"))
-#
+
+# print("Shape", data.X.shape[1], len(data.Xt))
+
+
 #
 # data >>= PCA()
 # print(data.id)
 # TODO  queue = None qnd descomenta acima
-my.update_remote(SQLite())
-SQLite().update_remote(MySQL(db="oka:kururu@localhost/oka"))
+# sq.update_remote(my)
+# my.update_remote(OkaSt(okatoken, alias="Iris", url=url))
+# SQLite().update_remote(MySQL(db="oka:kururu@localhost/oka"))
 
 
 def test_okast_id():
