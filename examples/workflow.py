c14e2d84be7afd0c5046c1107101d630fd7ed6b3
davips <dpsabc@gmail.com>
2020-10-25 09:17:59 -0300

    Add flag for lazyness in store/fetch

--- examples/workflow.py
+++ examples/workflow.py
@@ -4,7 +4,6 @@ import threading
 # import aiuna
 # exit()
 from aiuna.file import File
-from kururu.tool.communication.cache import Cache
 from kururu.tool.communication.log import Log
 from kururu.tool.communication.report import Report
 from kururu.tool.enhancement.binarize import Binarize
@@ -27,49 +26,49 @@ from util.create import user, token
 # from tatu.okast import OkaSt
 
 cfg = json.load(open("config.json"))
-my = MySQL(db=cfg["lmy"]["db"])
-
-# oka = OkaSt(okatoken, alias="Iris", url=url)
-sq = SQLite()
-# my = MySQL(db="oka:kururu@localhost/oka")
+# my = MySQL(db=cfg["lmy"]["db"])
 #
-wflow = (
-        File("iris.arff")
-        * Binarize
-        * Split
-        * PCA(n=3)
-        * Cache(PCA(n=3), storage=sq)
-        # * Cache(PCA(n=3), storage=oka)
-        * PCA(n=3)
-        * Log(">>>>>>>>>>>>>>>>> {X.shape} {inner.X.shape}")
-        * Report("{id}")
-        # * Cache(SVM2(C=0.25), storage=my)
-    # * Metric2
-    # * Report("tr {r}\t\tts {inner.r}")
-)
-
-data = wflow.data
-
-print("Data ID", data.id)
-
-
-# print("Shape", data.X.shape[1], len(data.Xt))
-
-
+# # oka = OkaSt(okatoken, alias="Iris", url=url)
+# sq = SQLite()
+# # my = MySQL(db="oka:kururu@localhost/oka")
+# #
+# wflow = (
+#         File("iris.arff")
+#         * Binarize
+#         * Split
+#         * PCA(n=3)
+#         * Cache(PCA(n=3), storage=sq)
+#         # * Cache(PCA(n=3), storage=oka)
+#         * PCA(n=3)
+#         * Log(">>>>>>>>>>>>>>>>> {X.shape} {inner.X.shape}")
+#         * Report("{id}")
+#         # * Cache(SVM2(C=0.25), storage=my)
+#     # * Metric2
+#     # * Report("tr {r}\t\tts {inner.r}")
+# )
+#
+# data = wflow.data
+#
+# print("Data ID", data.id)
+#
+#
+# # print("Shape", data.X.shape[1], len(data.Xt))
+#
+#
+# #
+# # data >>= PCA()
+# # print(data.id)
+# # TODO  queue = None qnd descomenta acima
+# # sq.update_remote(my)
+# # my.update_remote(OkaSt(okatoken, alias="Iris", url=url))
+# # SQLite().update_remote(MySQL(db="oka:kururu@localhost/oka"))
+#
 #
 # data >>= PCA()
-# print(data.id)
-# TODO  queue = None qnd descomenta acima
-# sq.update_remote(my)
-# my.update_remote(OkaSt(okatoken, alias="Iris", url=url))
-# SQLite().update_remote(MySQL(db="oka:kururu@localhost/oka"))
-
-
-data >>= PCA()
-print(" _________ d id:", data.id)
-
-p = data.parent_uuid
-print(" _________    children:", my.fetch_children(p), p.id)
+# print(" _________ d id:", data.id)
+#
+# p = data.parent_uuid
+# print(" _________    children:", my.fetch_children(p), p.id)
 
 def test_okast_id():
     from util.create import user, token
@@ -77,7 +76,7 @@ def test_okast_id():
     user = user("davips", "pass123", base_url=url)
     okatoken = token(**user, base_url=url)
     print("user created")
-    o = OkaSt(token=okatoken, url=url)
-    print(f"idddddd {o.id}")
+    # o = OkaSt(token=okatoken, url=url)
+    # print(f"idddddd {o.id}")
 
-# test_okast_id()
+test_okast_id()
