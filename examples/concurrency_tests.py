373d2fe4fe3f471ac17963c0c23883f9dad54a5d
davips <dpsabc@gmail.com>
2020-12-11 03:52:23 -0300

    Tatu(threaded).open() at custom Flask start to avoid concurrence issues Now closing Tatu at tests Better concurrency checker (but problem seems solved) Now threaded storages can be opened

--- examples/concurrency_tests.py
+++ examples/concurrency_tests.py
@@ -1,15 +1,22 @@
-from tatu.sql.mysql import MySQL
 import requests
+from simplejson import JSONDecodeError
 
+from aiuna.step.dataset import Dataset
+from tatu.sql.mysql import MySQL
 
 # Only tatu
 tatu = MySQL(db='tatu:kururu@localhost/tatu', threaded=False)
 
+tatu.store(Dataset().data, ignoredup=True)
+tatu.close()
+
+print("Lembrar de inserir um iris.arff pela web")
+
 
 def get_data():
-    data = tatu.fetch("3l9bSwFwL0TsSztkDb0iuVQ", lazy=False)
-    attrs = data.Xd
-    print("ATTRS>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", attrs)
+    data = tatu.fetch("06nk9kyCi8ywesyM8Pjw1jv", lazy=False)
+    _ = data.Xd
+    return data
 
 
 # Only SQLALchemy
@@ -24,8 +31,26 @@ headers = {'Authorization': 'Bearer ' + access_token}
 
 
 # Tatu and SQLAlchemy
-for i in range(10000):
-    # get_data()
-    response = requests.get(
-        'http://localhost:5000/api/posts/1', headers=headers)
-    print(i, response.json()['attrs'])
+
+
+import pathos.multiprocessing as mp
+
+
+def f(l):
+    try:
+        rs = []
+        for i in range(20):
+            # response = get_data().id
+            response = requests.get('http://localhost:5000/api/posts/1', headers=headers).json()
+            rs.append(response)
+    except JSONDecodeError:
+        return False
+    return rs
+
+
+pool = mp.Pool()
+rs = pool.map(f, [1, 2, 3])
+pool.close()
+pool.join()
+
+print("OK" if all(rs) else rs)
