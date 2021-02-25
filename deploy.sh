978c0f5a53593e4a8d1ccdf3c9f0b3e1e2df55bc
davips <dpsabc@gmail.com>
2021-02-25 19:35:44 -0300

    Disable RAM build

--- deploy.sh
+++ deploy.sh
@@ -10,18 +10,18 @@ printf "____FILES____\n" | tee -a ~/deploy_log.txt
 
 printf "____Backup current:____\n" | tee -a ~/deploy_log.txt
 rsync -a /oka-repository/ /oka-repository_bkp/ --delete &
-printf "____SSD -> to RAM:____\n" | tee -a ~/deploy_log.txt
-rsync -a /oka-repository/ /run/shm/oka-repository/ --delete
+# printf "____SSD -> to RAM:____\n" | tee -a ~/deploy_log.txt
+# rsync -a /oka-repository/ /run/shm/oka-repository/ --delete
 printf "____Backup current:____ FINISHED \n\n" | tee -a ~/deploy_log.txt
-printf "____SSD -> RAM:____ FINISHED \n\n" | tee -a ~/deploy_log.txt
+# printf "____SSD -> RAM:____ FINISHED \n\n" | tee -a ~/deploy_log.txt
 
 printf "____Starting with user:____\n" | tee -a ~/deploy_log.txt
 whoami | tee -a ~/deploy_log.txt
 printf "____Starting with user:____ FINISHED \n\n" | tee -a ~/deploy_log.txt
 
 printf "____Pulling from github____\n" | tee -a ~/deploy_log.txt
-#cd /home/oka/
-cd /run/shm/oka-repository/
+cd /home/oka/oka-repository/
+# cd /run/shm/oka-repository/
 git pull | tee -a ~/deploy_log.txt 
 printf "____Pulling from github____ FINISHED \n\n" | tee -a ~/deploy_log.txt
 
@@ -55,7 +55,6 @@ printf "____FRONTEND____\n" | tee -a ~/deploy_log.txt
 printf "____Install NPM packages____\n" | tee -a ~/deploy_log.txt
 cd ../frontend
 #npm set progress=false
-#npm i -g pnpm
 yarn install | tee -a ~/deploy_log.txt 
 printf "____Install NPM packages____ FINISHED \n\n" | tee -a ~/deploy_log.txt
 
@@ -68,14 +67,14 @@ printf "____FRONTEND____ FINISHED \n\n" | tee -a ~/deploy_log.txt
 
 
 
-printf "\n\n\n\n\n==============================================================================\n" | tee -a ~/deploy_log.txt
-printf "____DEPLOY FILES____\n" | tee -a ~/deploy_log.txt
+# printf "\n\n\n\n\n==============================================================================\n" | tee -a ~/deploy_log.txt
+# printf "____DEPLOY FILES____\n" | tee -a ~/deploy_log.txt
 
-printf "____RAM -> SSD:____\n" | tee -a ~/deploy_log.txt
-rsync -a /run/shm/oka-repository/ /oka-repository/ --delete
-printf "____RAM -> SSD:____ FINISHED \n\n" | tee -a ~/deploy_log.txt
+# printf "____RAM -> SSD:____\n" | tee -a ~/deploy_log.txt
+# rsync -a /run/shm/oka-repository/ /oka-repository/ --delete
+# printf "____RAM -> SSD:____ FINISHED \n\n" | tee -a ~/deploy_log.txt
 
-printf "____DEPLOY FILES____ FINISHED \n\n" | tee -a ~/deploy_log.txt
+# printf "____DEPLOY FILES____ FINISHED \n\n" | tee -a ~/deploy_log.txt
 
 
 
